ca325a51af42cccf0b45e663210bfbb2c6a9a2a6
Oğuzhan Ağır <90545225+oguzhanagir@users.noreply.github.com>
2022-09-11 15:02:16 +0300

    Actions Eklendi

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,43 @@
 import React from 'react'
+import {CssBaseLine,Container,Grid,AppBar,Toolbar,Typography,Button,IconButton} from "@mui/material";
+import {makeStyles} from "@mui/material/styles";
+import PenIcon from "@mui/material/icons/Create"
+const useStyles = makeStyles(theme=>({
+  root:{
+    flexGrow:1,
+
+  },
+  menuButton:{
+    marginRight:theme.spacing(2)
+  },
+  title:{
+    flexGrow:1,
+  },
+  container:{
+    marginTop: theme.spacing(3)
+  }
+}));
 
 const App = () => {
+  const classes = useStyles();
   return (
-    <div>
-      App is running
-
-    </div>
+    <>
+    <CssBaseLine/>
+    <Container maxWidth="lg">
+      <AppBar positon="static" color="inherit " elevation={0}>
+        <Toolbar>
+          <IconButton edge="start" className={classes.Container} color="inherit"/>
+        
+        <Typography variant="h6" color="secondary" className={classes.tite}>
+          <a href="http://localhost:3000/posts">Blogs</a>
+        </Typography>
+        <Button color="primary" variant="outlined" startIcon={<PenIcon/>}>
+          Yeni Blog
+        </Button>
+        </Toolbar>
+      </AppBar>
+    </Container>
+    </>
   )
 }
 
--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,5 @@
 import {legacy_createStore as createStore, applyMiddleware, compose} from "redux";
-import {thunk} from "redux-thunk";
+import thunk from "redux-thunk";
 import {composeWithDevTools} from "redux-devtools-extension";
 import rootReducer from "./reducers/rootReducer.js"
 
